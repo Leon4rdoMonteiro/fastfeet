55f5954eb06df7e1f530bbfeddf9da1a24c43942
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-18 17:01:17 -0300

    database updates

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,21 @@ import authMiddleware from './app/middlewares/auth';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
+// LOGIN ROUTE
 routes.post('/sessions', SessionController.store);
 
+// AUTH MIDDLEWARE
 routes.use(authMiddleware);
+
+// USER ROUTES
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+// RECIPIENT ROUTES
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
