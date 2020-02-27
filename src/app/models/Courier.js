7db61e567924756215b66e5064746245a8bc4354
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-27 18:19:55 -0300

    courier model/migration added

---
+++ src/app/models/Courier.js
@@ -0,0 +1,15 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Courier extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+            },
+            { sequelize }
+        );
+    }
+}
+
+export default Courier;
