2d61299744892f60c3840a8c6cd0f2ef0528be4f
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-03-02 19:36:49 -0300

    courier model updates

--- src/app/models/Delivery.js
+++ src/app/models/Delivery.js
@@ -5,10 +5,30 @@ class Delivery extends Model {
         super.init(
             {
                 product: Sequelize.STRING,
+                canceled_at: Sequelize.DATE,
+                start_date: Sequelize.DATE,
+                end_date: Sequelize.DATE,
             },
             { sequelize }
         );
         return this;
+    }
+
+    static associate(models) {
+        this.belongsTo(models.Recipient, {
+            foreignKey: 'recipient_id',
+            as: 'recipient',
+        });
+
+        this.belongsTo(models.User, {
+            foreignKey: 'deliveryman_id',
+            as: 'deliveryman',
+        });
+
+        this.belongsTo(models.File, {
+            foreignKey: 'signature_id',
+            as: 'signature',
+        });
     }
 }
 
