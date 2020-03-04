7d8e71d23ffc9967c7172c570c3f80ad1aeced27
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-03-04 12:02:56 -0300

    delivery problems migration created

---
+++ src/database/migrations/20200303191551-create-deliveries-problems.js
@@ -0,0 +1,31 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('delivery_problems', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            delivery_id: {
+                type: Sequelize.INTEGER,
+                references: { model: 'deliveries', key: 'id' },
+                allowNull: false,
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: queryInterface => {
+        return queryInterface.dropTable('delivery_problems');
+    },
+};
