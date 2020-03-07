509a1a6775a117812c0a82e5aaf73ae91b28e362
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-03-07 13:21:28 -0300

    Withdrawal controller added

---
+++ src/app/controllers/WithdrawalController.js
@@ -0,0 +1,98 @@
+import * as Yup from 'yup';
+import {
+    startOfDay,
+    endOfDay,
+    setSeconds,
+    setMinutes,
+    setHours,
+    format,
+    parseISO,
+    startOfHour,
+} from 'date-fns';
+import { Op } from 'sequelize';
+
+import Delivery from '../models/Delivery';
+
+import rangeHours from '../../utils/rangeHours';
+
+class WithdrawalController {
+    /**
+     *  Update delivery status => Start Date
+     */
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            start_date: Yup.number(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const delivery = await Delivery.findOne({
+            where: { id: req.params.id },
+        });
+
+        if (!delivery) {
+            return res.status(400).json({ error: 'Delivery does not exists' });
+        }
+
+        const orderAmount = await Delivery.count({
+            where: {
+                deliveryman_id: req.userId,
+                start_date: {
+                    [Op.between]: [
+                        startOfDay(new Date()),
+                        endOfDay(new Date()),
+                    ],
+                },
+            },
+        });
+
+        /**
+         * Verify of limit of withdrawals has been expired
+         */
+
+        if (orderAmount > 5) {
+            return res
+                .status(401)
+                .json({ error: 'Your daily withdrawal limit is over' });
+        }
+
+        /**
+         * Verify if start date is according to range hours
+         */
+
+        const available = rangeHours.map(time => {
+            const [hour, minute] = time.split(':');
+            const value = setSeconds(
+                setMinutes(setHours(new Date(), hour), minute),
+                0
+            );
+
+            return {
+                value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+            };
+        });
+
+        const start_date = {
+            value: format(startOfHour(new Date()), "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        };
+
+        const withdrawalAvailable = available.find(
+            a => a.value === start_date.value
+        );
+
+        if (!withdrawalAvailable) {
+            return res.status(401).json({
+                error: 'You cannot pick up orders outside bussiness hours',
+            });
+        }
+
+        await delivery.update({ start_date: parseISO(start_date.value) });
+
+        return res.json({ message: 'The product was successfully withdrawn' });
+    }
+}
+
+export default new WithdrawalController();
